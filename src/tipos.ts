b9e203c84e21a3db181ca1b7da0c6f98415c8aa3
David Vaquero <pepesan@gmail.com>
2023-05-20 16:29:21 +0200

    uso de var , let y const

--- src/tipos.ts
+++ src/tipos.ts
@@ -4,5 +4,20 @@ export function setupVars(element: HTMLParagraphElement){
     let c: number = 23
     let d: string = "Hola"
 
-    element.innerHTML=`nulo: ${a}, booleano ${b}, numero ${c}, cadena ${d}`;
+    // variables
+    // var se usa de una manera similar a Javascript
+    var numeroVariable: number = 2;
+    numeroVariable = numeroVariable +1;
+
+    // let
+    // se aplica a variables definidas en el bloque de código
+    let variableLocal = "local"
+    variableLocal = "otra local"
+
+    // const
+    // se aplica a constantes
+    const constante: string = "Adios"
+    // constante = "Adiosito"  No se puede cambiar
+
+    element.innerHTML=`nulo: ${a}, booleano ${b}, numero ${c}, cadena ${d}, numeroVariable ${numeroVariable}, variableLocal ${variableLocal}, constante ${constante}`;
 }
